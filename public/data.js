64bcd3d96480dde5c83531a45ba4c85bc5cb216e
Mauricio <mauri.yapura@yahoo.com>
2021-06-27 20:49:37 -0300

    First version

---
+++ public/data.js
@@ -0,0 +1,76 @@
+
+const d = document;
+
+const host = "http://localhost:5000/";
+d.querySelector("#get-video-info-btn").addEventListener("click",()=>{
+    
+    let videoURL = d.querySelector("#videoURL").value.trim();    
+    if(videoURL.length == 0){
+        alert("Please enter Youtube video link");
+        return;
+    }    
+    fetch(host+"videoInfo?videoURL="+videoURL).then((res)=>{       
+        return res.json();
+    }).then((data)=>{
+        console.log(data);
+        let detailsNode = {
+            thumbnail: d.querySelector(".video-data .thumbnail img"),
+            title: d.querySelector(".video-data .info h2"),
+            description: d.querySelector(".video-data .info p"),
+            videoURL: d.querySelector(".video-data .controls #video-url"),
+            downloadOptions: d.querySelector(".video-data .controls #download-options")
+        };
+        
+        let html = "";
+        for(let i=0; i<data.formats.length; i++){         
+            if( data.formats[i].container != "mp4"){
+                continue;
+            }
+
+            if ( data.formats[i].qualityLabel == null){
+                continue;
+            }
+
+            if(data.formats[i].audioCodec == null){
+                continue;
+            }            
+            //console.log(i)
+            html += `
+						<option value="${data.formats[i].itag}">
+							${data.formats[i].container} - ${data.formats[i].qualityLabel}
+						</option>
+					`;          
+            detailsNode.downloadOptions.innerHTML = html;              
+        };
+                   
+        console.log(html);        
+        detailsNode.thumbnail.src = data.videoDetails.thumbnails[data.videoDetails.thumbnails.length - 1].url; 
+        detailsNode.title.innerText = data.videoDetails.title;
+        detailsNode.description.innerText = data.videoDetails.description;
+        detailsNode.videoURL.value = videoURL;
+        d.querySelector(".video-data").style.display = "block";
+        d.querySelector(".video-data .controls #download-options").scrollIntoView({
+            behavior: "smooth"
+        });
+
+    }).catch((err)=>{
+        alert("Something went wrong");
+    })
+});
+
+d.querySelector("#download-btn").addEventListener("click", ()=>{
+    let videoURL = d.querySelector("#video-url").value;
+    let itag = d.querySelector("#download-options").value;
+    window.open(host + "download?videoURL=" + videoURL + "&itag="+itag);
+})
+
+
+
+
+
+
+
+
+
+
+
